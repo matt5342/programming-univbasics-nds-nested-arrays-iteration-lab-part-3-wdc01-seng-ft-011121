418eafbe7a39399188663a151ab7511d65669c48
Matthew Sewell <mattfsewell@gmail.com>
2020-11-23 17:00:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,20 @@
 def join_nested_strings(src)
   
   final_string = ""
+  string_array = []
+  outer = 0
   
+  while outer < src.count do
+    inner = 0
+    while inner < src[outer].count do
+      if src[outer][inner].class == String
+        string_array << src[outer][inner]
+      end
+      inner += 1
+    end
+    outer += 1
+  end
+  final_string = string_array.join(' ')
+  final_string
   
 end
